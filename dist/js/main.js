9dff55d090faececbffa7d236c7bb781f8713c20
webmaker <work@webmaker.work>
2017-03-09 20:49:45 +0200

    004

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 
 let circleLength = 628.318,
 myCircleBox = $('.circle-box'),
-nullPercent = 0;
+nullPercent = 0,
+mySVG = '<svg viewBox="0 0 220 220"><circle class="circle__a" cx="110" cy="110" r="100"/><circle class="circle__b" cx="110" cy="110" r="110"/></svg>',
+myDivPercent = '<div class="percent"></div>';
 
-// SVG element
-myCircleBox.html('<svg viewBox="0 0 220 220"><circle class="circle__a" cx="110" cy="110" r="100"/><circle class="circle__b" cx="110" cy="110" r="110"/></svg><div class="percent"></div>');
+myCircleBox.html(mySVG + myDivPercent);
 
 $('.percent').text(nullPercent + '%');
 
@@ -13,15 +14,16 @@ $('.circle__a').css({'stroke-dasharray' : circleLength, 'stroke-dashoffset' : ci
 
 $('.circle-box').each(function(index) {
 	let that = $(this);
-			myPercent = $(this).attr('data-percent'),
-			toPercent = circleLength - circleLength / 100 * myPercent;
-	that.find('.percent').animate({num: myPercent}, {
-		duration: 1500,
-		step: function (num){
-			$(this).text((num).toFixed(0) + '%');
-		}
-	});
-	that.find('.circle__a').animate({'stroke-dashoffset' : toPercent}, 1500);
+		myPercent = +that.attr('data-percent'),
+		myTime = +that.attr('data-time'),
+		toPercent = circleLength - circleLength / 100 * myPercent;
+		that.find('.percent').animate({num: myPercent}, {
+			duration: myTime,
+			step: function (num){
+				$(this).text((num).toFixed(0) + '%');
+			}
+		});
+	that.find('.circle__a').animate({'stroke-dashoffset' : toPercent}, myTime);
 });
 
 });
